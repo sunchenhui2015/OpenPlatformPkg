4d3cdc09becd5e280b5fac717352dbdecf3e489f
Zhangfei Gao <zhangfei.gao@linaro.org>
2016-06-07 14:35:02 +0800

    Revert "D02/SAS: need more delay in polling for autoboot"
    This reverts commit eac650a0dc051d2a8614558a8c3bb705fa590e83.

--- Chips/Hisilicon/Drivers/SasV1Dxe/SasV1Dxe.c
+++ Chips/Hisilicon/Drivers/SasV1Dxe/SasV1Dxe.c
@@ -433,7 +433,7 @@ STATIC EFI_STATUS prepare_cmd (
   while (slot->used) {
     if (READ_REG32(base, OQ_INT_SRC) & BIT(queue)) {
       // Required for data get ready in polling mode
-      MicroSecondDelay(2000);
+      MicroSecondDelay(500);
       break;
     }
     //Waiting slot->used change
